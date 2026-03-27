f4c573d9356d0c59aeef8e6c782a1c895f0f1215
Jakub Szymczyk <kubaszymczyk231@gmail.com>
2026-03-27 19:23:11 +0100

    created pieces and board

---
+++ main.py
@@ -0,0 +1,67 @@
+import numpy as np
+from pieces import Pawn, King, Knight, Queen, Bishop, Rook, Piece
+
+
+
+class ChessBoard:
+    def __init__(self) -> None:
+        self._board = np.empty((8, 8), dtype=object)
+        self._initial_positions = {
+            (0, 0): {"class": Rook, "color": "black"},
+            (0, 1): {"class": Knight, "color": "black"},
+            (0, 2): {"class": Bishop, "color": "black"},
+            (0, 3): {"class": Queen, "color": "black"},
+            (0, 4): {"class": King, "color": "black"},
+            (0, 5): {"class": Bishop, "color": "black"},
+            (0, 6): {"class": Knight, "color": "black"},
+            (0, 7): {"class": Rook, "color": "black"},
+            (1, 0): {"class": Pawn, "color": "black"},
+            (1, 1): {"class": Pawn, "color": "black"},
+            (1, 2): {"class": Pawn, "color": "black"},
+            (1, 3): {"class": Pawn, "color": "black"},
+            (1, 4): {"class": Pawn, "color": "black"},
+            (1, 5): {"class": Pawn, "color": "black"},
+            (1, 6): {"class": Pawn, "color": "black"},
+            (1, 7): {"class": Pawn, "color": "black"},
+
+            (7, 0): {"class": Rook, "color": "white"},
+            (7, 1): {"class": Knight, "color": "white"},
+            (7, 2): {"class": Bishop, "color": "white"},
+            (7, 3): {"class": Queen, "color": "white"},
+            (7, 4): {"class": King, "color": "white"},
+            (7, 5): {"class": Bishop, "color": "white"},
+            (7, 6): {"class": Knight, "color": "white"},
+            (7, 7): {"class": Rook, "color": "white"},
+            (6, 0): {"class": Pawn, "color": "white"},
+            (6, 1): {"class": Pawn, "color": "white"},
+            (6, 2): {"class": Pawn, "color": "white"},
+            (6, 3): {"class": Pawn, "color": "white"},
+            (6, 4): {"class": Pawn, "color": "white"},
+            (6, 5): {"class": Pawn, "color": "white"},
+            (6, 6): {"class": Pawn, "color": "white"},
+            (6, 7): {"class": Pawn, "color": "white"},
+        }
+
+    def board(self) -> np.ndarray:
+        return self._board
+    
+    def set_piece(self, pos: tuple[int, int], piece: Piece) -> None:
+        board = self.board()
+        board[pos[0], pos[1]] = piece
+    
+    def initial_positions(self) -> dict:
+        return self._initial_positions
+    
+    def populate(self) -> None:
+        for pos, keys in self.initial_positions().items():
+            piece_class: Piece = keys["class"]
+            color = keys["color"]
+            
+            piece_object = piece_class(color, pos)
+            self.set_piece(pos, piece_object)
+
+
+if __name__ == "__main__":
+    chess_board = ChessBoard()
+    chess_board.populate()
+    print(chess_board.board())
---
+++ pieces.py
@@ -0,0 +1,135 @@
+class Piece:
+    def __init__(self, color: str, start_pos: tuple[int, int]):
+        self._color = color
+        self._start_pos = start_pos
+        self._pos = start_pos
+        
+    def color(self) -> str:
+        return self._color
+    
+    def x(self) -> int:
+        return self._pos[0]
+    
+    def y(self) -> int:
+        return self._pos[1]
+    
+    def offset(self, offset: tuple[int, int]) -> tuple[int, int]:
+        return (self.x() + offset, self.y() + offset)
+    
+    def movement(self) -> list[tuple[int, int]]:
+        raise NotImplementedError
+    
+    def start_pos(self) -> tuple[int, int]:
+        return self._start_pos
+    
+    def current_pos(self) -> tuple[int, int]:
+        return self._pos
+    
+    def filter_moves(self, moves: list[tuple[int, int]]) -> list[tuple[int, int]]:
+        current_pos = self.current_pos()
+        return [move for move in moves if (0 <= move[0] <= 7 and 0 <= move[1] <= 7 and move != current_pos)]
+    
+class Pawn(Piece):
+    def __init__(self, color: str, start_pos: tuple[int, int]) -> None:
+        super().__init__(color, start_pos)
+        self._dy = -1 if self.color() == "black" else 1
+        
+    def dy(self) -> int:
+        return self._dy
+        
+    def movement(self) -> list[tuple[int, int]]:
+        possible_moves = [
+            self.offset(0, self.dy()),
+            self.offset(-1, self.dy()),
+            self.offset(1, self.dy())
+        ]
+        
+        if self.start_pos() == self.current_pos():
+            possible_moves.append(self.offset(0, self.dy() * 2))
+            
+        return self.filter_moves(possible_moves)
+    
+    def __repr__(self) -> str:
+        return f"{self.color()} Pawn"
+    
+    
+class Rook(Piece):
+    def __init__(self, color: str, start_pos: tuple[int, int]) -> None:
+        super().__init__(color, start_pos)
+        
+    def movement(self) -> list[tuple[int, int]]:
+        possible_x = [self.offset(offset, 0) for offset in range(-8, 9, 1)]
+        possible_y = [self.offset(0, offset) for offset in range(-8, 9, 1)]
+        
+        possible_moves = possible_x + possible_y
+        return self.filter_moves(possible_moves)
+    
+    def __repr__(self) -> str:
+        return f"{self.color()} Rook"
+    
+class Knight(Piece):
+    def __init__(self, color: str, start_pos: tuple[int, int]) -> None:
+        super().__init__(color, start_pos)
+        
+    def movement(self) -> list[tuple[int, int]]:
+        possible_moves = [
+            self.offset(-1, 2),
+            self.offset(-2, 1),
+            self.offset(-2, -1),
+            self.offset(-1, -2),
+            self.offset(1, -2),
+            self.offset(2, -1),
+            self.offset(2, 1),
+            self.offset(1, 2)
+        ]
+        
+        return self.filter_moves(possible_moves)
+    
+    def __repr__(self):
+        return f"{self.color()} Knight"
+    
+class Bishop(Piece):
+    def __init__(self, color: str, start_pos: tuple[int, int]) -> None:
+        super().__init__(color, start_pos)
+        
+    def movement(self):
+        possible_1 = [self.offset(-offset, offset) for offset in range(-8, 9, 1)]
+        possible_2 = [self.offset(offset, -offset) for offset in range(-8, 9, 1)]
+
+        return self.filter_moves(possible_1 + possible_2)
+    
+    def __repr__(self) -> str:
+        return f"{self.color()} Bishop"
+    
+class Queen(Bishop):
+    def __init__(self, color: str, start_pos: tuple[int, int]) -> None:
+        super().__init__(color, start_pos)
+        
+    def movement(self):
+        possible_x = [self.offset(offset, 0) for offset in range(-8, 9, 1)]
+        possible_y = [self.offset(0, offset) for offset in range(-8, 9, 1)]
+        return super().movement() + self.filter_moves(possible_x + possible_y)
+    
+    def __repr__(self) -> str:
+        return f"{self.color()} Queen"
+    
+class King(Piece):
+    def __init__(self, color: str, start_pos: tuple[int, int]) -> None:
+        super().__init__(color, start_pos)
+        
+    def movement(self):
+        possible_moves = [
+            self.offset(1, 0),
+            self.offset(1, 1),
+            self.offset(0, 1),
+            self.offset(-1, 1),
+            self.offset(-1, 0),
+            self.offset(-1, -1),
+            self.offset(0, -1),
+            self.offset(1, -1)
+        ]
+        
+        return self.filter_moves(possible_moves)
+    
+    def __repr__(self) -> str:
+        return f"{self.color()} King"
